7d5a1d889033dcde2e4474e99a18c06a5fd450d3
leeeel <libo.0930@bytedance.com>
2023-05-07 02:22:01 +0800

    add solution

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
 class [[maybe_unused]] Solution1487 {
 public:
     static string addSuffix(string str, int num) {
@@ -79,7 +93,67 @@ public:
         // [0, 0]
         res += (long) mp[0] * (mp[0] - 1) / 2;
 
-        return (int)res;
+        return (int) res;
+    }
+};
+
+class [[maybe_unused]] Solution513 {
+public:
+    [[maybe_unused]] static int findBottomLeftValue(TreeNode *root) {
+        queue<TreeNode *> q;
+        int res;
+
+        q.push(root);
+        res = root->val;
+
+        while (!q.empty()) {
+            unsigned int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                auto temp = q.front();
+                q.pop();
+
+                if (temp->right != nullptr) {
+                    q.push(temp->right);
+                }
+                if (temp->left != nullptr) {
+                    q.push(temp->left);
+                }
+
+                res = temp->val;    // right -> left
+            }
+        }
+
+        return res;
+    }
+};
+
+class [[maybe_unused]] Solution165 {
+public:
+    [[maybe_unused]] static int compareVersion(string version1, string version2) {
+        int i = 0, j = 0;
+        unsigned int m = version1.size(), n = version2.size();
+
+        while (i < m || j < n) {
+            unsigned int num1 = 0, num2 = 0;
+
+            for (; i < m && version1[i] != '.'; ++i) {
+                num1 = num1 * 10 + version1[i] - '0';
+            }
+            for (; j < n && version2[j] != '.'; ++j) {
+                num2 = num2 * 10 + version2[j] - '0';
+            }
+
+            if (num1 < num2) {
+                return -1;
+            } else if (num1 > num2) {
+                return 1;
+            }
+
+            i++, j++;
+        }
+
+        return 0;
     }
 };
 
