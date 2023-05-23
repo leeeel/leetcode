9898469943f3a3a362e958d7a57740440a3009a1
leeeel <libo.0930@bytedance.com>
2023-05-24 01:48:49 +0800

    add solution

--- volume4.cpp
+++ volume4.cpp
@@ -75,6 +75,128 @@ public:
     }
 };
 
+class [[maybe_unused]] Solution979 {
+private:
+    int res = 0;
+
+    int dfs(TreeNode *root) {
+        if (root == nullptr) {
+            return 0;
+        }
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+        res += abs(left) + abs(right);
+
+        return left + right + root->val - 1;
+    }
+
+public:
+    [[maybe_unused]] int distributeCoins(TreeNode *root) {
+        dfs(root);
+
+        return res;
+    }
+};
+
+class [[maybe_unused]] Solution968 {
+private:
+    int res = 0;
+
+    int dfs(TreeNode *root) {
+        if (root == nullptr) {
+            return 1;
+        }
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        // 0 -- not covered, 1 -- covered, 2 -- camera
+        if (left == 0 || right == 0) {
+            res++;
+            return 2;
+        } else if (left == 2 || right == 2) {
+            return 1;
+        } else if (left == 1 && right == 1) {
+            return 0;
+        }
+
+        return -1;
+    }
+
+public:
+    [[maybe_unused]] int minCameraCover(TreeNode *root) {
+        if (dfs(root) == 0) {
+            res++;
+        }
+
+        return res;
+    }
+};
+
+class [[maybe_unused]] Solution662 {
+public:
+    [[maybe_unused]] static int widthOfBinaryTree(TreeNode *root) {
+        if (root == nullptr) {
+            return 0;
+        }
+
+        unsigned int res = 1;
+        vector<pair<TreeNode *, unsigned int>> arr;
+        arr.emplace_back(root, 1);
+
+        while (!arr.empty()) {
+            int n = int(arr.size());
+            res = max(res, arr[n - 1].second - arr[0].second + 1);
+
+            for (int i = 0; i < n; ++i) {
+                auto [node, index] = arr[i];
+
+                if (node->left != nullptr) {
+                    arr.emplace_back(node->left, index * 2);
+                }
+                if (node->right != nullptr) {
+                    arr.emplace_back(node->right, index * 2 + 1);
+                }
+            }
+
+            arr.erase(arr.begin(), arr.begin() + n);
+        }
+
+        return int(res);
+    }
+};
+
+class [[maybe_unused]] Solution90 {
+private:
+    vector<vector<int>> res;
+    vector<int> subset;
+    unsigned int n;
+
+    void dfs(vector<int> &nums, int startIdx) {
+        res.emplace_back(subset);
+
+        for (int i = startIdx; i < n; ++i) {
+            if (i != startIdx && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            subset.emplace_back(nums[i]);
+            dfs(nums, i + 1);
+            subset.pop_back();
+        }
+    }
+
+public:
+    [[maybe_unused]] vector<vector<int>> subsetsWithDup(vector<int> &nums) {
+        n = nums.size();
+        sort(nums.begin(), nums.end());
+        dfs(nums, 0);
+
+        return res;
+    }
+};
+
 [[maybe_unused]] int main() {
     return 0;
 }
