f9bdd9a3038fb9a6bfb171035ff4dd517abdc149
lee <libo.0930@bytedance.com>
2023-08-03 14:34:58 +0800

    add new file

--- volume6.cpp
+++ volume6.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+class [[maybe_unused]] Solution827 {
+public:
+    [[maybe_unused]] static int largestIsland(vector<vector<int>> &grid) {
+        return 0;
+    }
+};
+
 [[maybe_unused]] int main() {
     return 0;
 }
