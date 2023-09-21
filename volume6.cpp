b6adafcdf6e2737267e3907232f771a9b689e357
lee <libo.0930@bytedance.com>
2023-09-21 14:55:28 +0800

    fix sth

--- volume6.cpp
+++ volume6.cpp
@@ -15,25 +15,25 @@ private:
         return true;
     }
 
-    int dfs(vector<vector<int>> &grid, vector<vector<bool>> &visited, int x, int y, int &tag) {
-        if (visited[x][y] || grid[x][y] == 0) {
+    int dfs(vector<vector<int>> &grid, int x, int y, int tag) {
+        if (grid[x][y] != 1) {
             return 0;
         }
 
         unsigned int n = grid.size();
         int res = 1;
 
-        visited[x][y] = true, grid[x][y] = tag;
+        grid[x][y] = tag;
 
-        for (auto dir: dirs) {
-            int newX = x + dir[0];
-            int newY = y + dir[1];
+        for (int i = 0; i < 4; i++) {   // Using auto for iteration will result in copying operations, which can slow down the program.
+            int newX = x + dirs[i][0];
+            int newY = y + dirs[i][1];
 
             if (!isValid(newX, newY, n)) {
                 continue;
             }
 
-            res += dfs(grid, visited, newX, newY, tag);
+            res += dfs(grid, newX, newY, tag);
         }
 
         return res;
@@ -45,7 +45,6 @@ public:
         int tag = 2;
         int res = 0;
         bool noZero = true;
-        vector<vector<bool>> visited = vector<vector<bool>>(n, vector<bool>(n, false));
         unordered_map<int, int> islandAreaMap;
 
 
@@ -55,9 +54,10 @@ public:
                     noZero = false;
                 }
 
-                if (!visited[i][j] && grid[i][j] == 1) {
-                    int islandArea = dfs(grid, visited, i, j, tag);
+                if (grid[i][j] == 1) {
+                    int islandArea = dfs(grid, i, j, tag);
                     islandAreaMap[tag++] = islandArea;
+                    res = max(res, islandArea);
                 }
             }
         }
@@ -72,16 +72,16 @@ public:
                 unordered_set<int> countedIslandSet;
 
                 if (grid[i][j] == 0) {
-                    for (auto dir: dirs) {
-                        int newX = i + dir[0];
-                        int newY = j + dir[1];
+                    for (int k = 0; k < 4; k++) {   // Using auto for iteration will result in copying operations, which can slow down the program.
+                        int newX = i + dirs[k][0];
+                        int newY = j + dirs[k][1];
 
                         if (!isValid(newX, newY, n)) {
                             continue;
                         }
 
                         int currTag = grid[newX][newY];
-                        if (countedIslandSet.count(currTag)) {
+                        if (currTag == 0 || countedIslandSet.count(currTag)) {
                             continue;
                         }
 
