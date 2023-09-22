f0328018126ca3c30898ed2af4ee1002b24c71c5
lee <libo.0930@bytedance.com>
2023-09-22 17:14:29 +0800

    add solution

--- volume6.cpp
+++ volume6.cpp
@@ -25,7 +25,7 @@ private:
 
         grid[x][y] = tag;
 
-        for (int i = 0; i < 4; i++) {   // Using auto for iteration will result in copying operations, which can slow down the program.
+        for (int i = 0; i < 4; i++) {// Using auto for iteration will result in copying operations, which can slow down the program.
             int newX = x + dirs[i][0];
             int newY = y + dirs[i][1];
 
@@ -72,7 +72,7 @@ public:
                 unordered_set<int> countedIslandSet;
 
                 if (grid[i][j] == 0) {
-                    for (int k = 0; k < 4; k++) {   // Using auto for iteration will result in copying operations, which can slow down the program.
+                    for (int k = 0; k < 4; k++) {// Using auto for iteration will result in copying operations, which can slow down the program.
                         int newX = i + dirs[k][0];
                         int newY = j + dirs[k][1];
 
@@ -98,6 +98,106 @@ public:
     }
 };
 
+class [[maybe_unused]] DiningPhilosophers {
+private:
+    mutex lock[5];
+
+public:
+    DiningPhilosophers() = default;
+
+    [[maybe_unused]] void wantsToEat(int philosopher,
+                                     const function<void()> &pickLeftFork,
+                                     const function<void()> &pickRightFork,
+                                     const function<void()> &eat,
+                                     const function<void()> &putLeftFork,
+                                     const function<void()> &putRightFork) {
+        int leftChopstick = philosopher;
+        int rightChopstick = (philosopher + 1) % 5;
+
+        if (philosopher % 2 == 0) {
+            lock[leftChopstick].lock();
+            lock[rightChopstick].lock();
+            pickLeftFork();
+            pickRightFork();
+        } else {
+            lock[rightChopstick].lock();
+            lock[leftChopstick].lock();
+            pickLeftFork();
+            pickRightFork();
+        }
+
+        eat();
+        putLeftFork();
+        putRightFork();
+        lock[leftChopstick].unlock();
+        lock[rightChopstick].unlock();
+    }
+};
+
+class [[maybe_unused]] Solution68 {
+private:
+    static string genBlankStr(int n) {
+        string res;
+
+        return res.assign(n, ' ');
+    }
+
+    static string joinStr(vector<string> &words, int left, int right, const string &sepStr) {
+        string res = words[left];
+
+        for (int i = left + 1; i < right; ++i) {
+            res.append(sepStr);
+            res.append(words[i]);
+        }
+
+        return res;
+    }
+
+public:
+    [[maybe_unused]] static vector<string> fullJustify(vector<string> &words, int maxWidth) {
+        vector<string> res;
+        int right = 0, n = int(words.size());
+        int left;
+
+        while (true) {
+            left = right;
+            int curLineWordsWidth = 0;
+
+            while (right < n && curLineWordsWidth + words[right].length() + right - left <= maxWidth) {
+                curLineWordsWidth += int(words[right++].length());
+            }
+
+            if (right == n) {
+                string s = joinStr(words, left, right, " ");
+                res.emplace_back(s.append(genBlankStr(maxWidth - int(s.length()))));
+
+                return res;
+            }
+
+            int curLineWordsNum = right - left;
+            int curLineBlanksNum = maxWidth - curLineWordsWidth;
+
+            if (curLineWordsNum == 1) {
+                string s = words[left];
+                res.emplace_back(s.append(genBlankStr(maxWidth - int(s.length()))));
+
+                continue;
+            }
+
+            int curLineAvgBlanksNum = curLineBlanksNum / (curLineWordsNum - 1);
+            int curLineExactBlanksNum = curLineBlanksNum % (curLineWordsNum - 1);
+
+            string s1 = joinStr(words, left, left + curLineExactBlanksNum + 1, genBlankStr(curLineAvgBlanksNum + 1));
+            string s2 = joinStr(words, left + curLineExactBlanksNum + 1, right, genBlankStr(curLineAvgBlanksNum));
+            s1.append(genBlankStr(curLineAvgBlanksNum));
+            s1.append(s2);
+
+            res.emplace_back(s1);
+        }
+    }
+};
+
 [[maybe_unused]] int main() {
+
     return 0;
 }
